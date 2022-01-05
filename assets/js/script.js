1b0dcc35beabf0a6380910fa6907b5b110d2f688
wyork <w.york63@gmail.com>
2022-01-05 17:28:14 -0500

    indivdual weather updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var loadCities = function () {
             for (var i = 0; i < citiesArray.length; i++) {
                 if (citiesArray[i] === lastSearch) {
                     existingButton = citiesArray[i];
-                    break
+                    break;
                 }
             }
             if (!existingButton) {
@@ -46,7 +46,7 @@ var loadCities = function () {
 
 // when search button is clicked - it finds input for that location
 var formSubmitHandler = function(event) {
-    event.preventdefault();
+    event.preventDefault();
     var city = cityInputEl.value.trim();
     if (city) {
         getLatLon(city);
@@ -111,7 +111,7 @@ var getWeather = function(lat, lon, city) {
 var displayCurrentWeather = function(data, city) {
     console.log(data);
     // clear results
-    resultsContainerEl.textContent = "";
+    resultsContainerEl.textContent = '';
 
     // add container for new weather
     var currentWeatherEl = document.createElement('article');
@@ -119,17 +119,33 @@ var displayCurrentWeather = function(data, city) {
     currentWeatherEl.classList = "p-10";
 
     // create h2 with name and date
+    var h2El = document.createElement('h2');
+    h2El.id = 'city-name';
+    var today = new Date();
+    // function to create the date (+1 becasue get month pulls from array)
+    var date = (today.getMonth() + 1) + '/' + 
+        today.getDate() + '/' + 
+        today.getFullYear();
+    h2El.textContent = city + ' (' + date + ')  ';
+
+    // Create weather icon to the right of h2
+    var icon = data.weather[0].icon;
+    var imgEl = document.createElement('img')
+    imgEl.classList = 'absolute-position';
+    imgEl.setAttribute('src', 'http://openweathermap.org/img/w/' + icon + '.png')
+
 
     // create paragraphs for temp wind and humidity 
     var tempEl = document.createElement('p')
-    tempEl.innterHTML = 'Temp: ' + data.tempp + '<span>&#176;</span>F'
+    tempEl.innerHTML = 'Temp: ' + data.temp + '<span>&#176;</span>F';
+    tempEl.classList = 'p-20-0-10-0';
 
     var windEl = document.createElement('p')
     windEl.textContent = 'Wind: ' + data.wind_speed + ' MPH';
-    windEl.classList = 'p-20-0-10-0';
+    windEl.classList = 'p-10-0';
 
     var humidityEl = document.createElement('p')
-    humidityEl.textContent = 'Humidity: ' +data.humidity + '%'; 
+    humidityEl.textContent = 'Humidity: ' + data.humidity + '%'; 
     humidityEl.classList = 'P-10-0';
     
     var uvIndexEl = document.createElement('p')
@@ -149,19 +165,63 @@ var displayCurrentWeather = function(data, city) {
 }
 
 // displat forecast function 
+var displayForecast = function(data) {
+    console.log(data);
+    // will create container for forecast
+    var forecastEl = document.createElement('div');
 
-// will create container for forecast
+    var h3El = document.createElement('h3');
+    h3El.textContent = '5-Day Forecast';
+    h3El.classList = 'p-20-0-10-0';
+    
+    var daysContainerEl = document.createElement('div');
+    daysContainerEl.classList = 'row space-between'
 
-//function to add current date
+    for (var i = 1; i < 6; i++) {
+        var singleDayEl = document.createElement('article');
+        singleDayEl.classList = 'col-5 col-xl-2 p-10 dark-bg mb-20';
 
-//add weather icon
+    //function to add current date
+    var date = new Date(data[i].dt * 1000);
+    var fullDate = (date.getMonth() + 1) + '/' +
+        date.getDate() + '/' +
+        date.getFullYear();
+    var h4El = document.createElement('h4');
+    h4El.textContent = fullDate;
+    h4El.classList = 'white-font';
+    
+    var icon = data[i].weather[0].icon;
+    var imgEl = document.createElement('img');
+    imgEl.setAttribute('src', 'http://openweathermap.org/img/w/' + icon + '.png');
+    
+    // create paragraph for temp wind humidiy and uvi
+    var tempEl = document.createElement('p');
+    tempEl.innerHTML = 'Temp: ' + data[i].temp.day + ' <span>&#176;</span>F';
+    tempEl.classList = 'p-10-0 white-font lg-font-size';
 
-// create paragraph for temp wind humidiy and uvi
+    var windEl = document.createElement('p')
+    windEl.textContent = 'Wind: ' + data[i].wind_speed + ' MPH';
+    windEl.classList = 'p-10-0 white-font lg-font-size'
 
+    var humidityEl = document.createElement('p')
+    humidityEl.textContent = 'Humidity: ' + data[i].humidity + ' %';
+    humidityEl.classList = 'P-10-0 white-font lg-font-size';
+
+    singleDayEl.appendChild(h4El);
+    singleDayEl.appendChild(imgEl);
+    singleDayEl.appendChild(tempEl);
+    singleDayEl.appendChild(windEl);
+    singleDayEl.appendChild(humidityEl);
+
+    daysContainerEl.appendChild(singleDayEl);
+    }
 
 // add all the elements to current weather section
-
+forecastEl.appendChild(h3El);
+forecastEl.appendChild(daysContainerEl);
 //add curent weather section to results
+resultsContainerEl.appendChild(forecastEl);
+}
 
 //load info from previous search history when page starst
 
