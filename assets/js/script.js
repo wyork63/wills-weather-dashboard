25f8feee7cd324aa014d8cc3db82723e3062829f
wyork <w.york63@gmail.com>
2022-01-05 00:03:18 -0500

    updating js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,43 @@ var resultsContainerEl = document.querySelector('#results');
 
 // if last search is not an existing button then add it to array 
 
+// when search button is clicked - it finds input for that location
+var formSubmitHandler = function(event) {
+    event.preventdefault();
+    var city = cityInputEl.value.trim();
+    if (city) {
+        getLatLon(city);
+        cityInputEl.value = '';
+    }
+    else {
+        alert('Please enter the name of a city');
+    }
+};
+
+// add function when an old search button is clicked
+
+
+// get latitude and longitude of the city and pass it into the argument for getWeather() 
+var getLatLon = function(city) {
+    // apikey from weather site
+    var apiKey = "ff8f64c1c4ae73a70e4b2c346addc528";
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    localStorage.setItem('last-search', data.name)
+                    loadCities();
+                    var city = data.name;
+                    var lat = data.coord.lat;
+                    var lon = data.coord.lon;
+                    getWeather (lat, lon, city);
+                });
+            } else {
+                alert('Error: No city Found');
+            }
+        })
+        .catch(function(error) {
+            alert('Unable to Connect')
+        });
+};
