849f8eccba993574191ea6f1c773064634c834c9
wyork <w.york63@gmail.com>
2022-01-04 22:46:08 -0500

    starting css html and some js pseudocodde

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var searchFormEl = document.querySelector('#search-form');
+var previousSearchesEl = document.querySelector('#city-buttons');
+var cityInputEl = document.querySelector('#city-input');
+var resultsContainerEl = document.querySelector('#results');
+
+// function where if a search has been done in this browser 
+
+// then pull city from city array 
+
+// and display it into buttons based on array
+
+// if last search is not an existing button then add it to array 
+
